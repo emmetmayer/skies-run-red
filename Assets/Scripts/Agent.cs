b97fae3b77c496018e397badb698fdce63329ebf
rufob <66888072+rufob@users.noreply.github.com>
2023-03-10 14:52:39 -0500

    some basic stuff

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -8,6 +8,15 @@ public class Agent
     [SerializeField] public int TeamID {get; private set;}
     [SerializeField] public GameObject Character {get; private set;}
 
+    private float Health;
+    //theres probably multiple of both of these?
+    [Tooltip("When this collides with the enemy's hitbox they take damage")]
+    private Collider _hurtBox;
+    [Tooltip("When this collides with the enemy's hurtbox I take damage")]
+    private Collider _hitBox;
+
+    private bool isDefending;
+    
     public Agent(string _name, int _teamID)
     {
         Name = _name;
@@ -22,15 +31,45 @@ public class Agent
             GameObject.Destroy(Character);
         }
         Character = GameObject.Instantiate(Resources.Load("Character", typeof(GameObject)), GameObject.Find("Agents").transform) as GameObject;
+        //grab all the relevant colliders here
     }
 
     private void Spawn()
     {
         TeamService.Instance.SpawnAgent(this);
+        //should this run LoadCharacter here?
     }
 
     public override string ToString()
     {
         return Name;
     }
+
+    //return true if damage caused player to die
+    public bool TakeDamage(int n)
+    {
+        if (isDefending)
+        {
+            //process attack differently
+        }
+
+        if(n > 0) Health -= n;
+        if (Health < 0)
+        {
+            HandleDeath();
+            return true;
+        }
+
+        return false;
+    }
+
+    public void HandleDeath()
+    {
+        //spawn timer
+        //body ragdoll?
+        //reset values
+    }
+    
+    //gotta figure out how to do collision events?
+    
 }
